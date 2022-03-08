e75d91f10e64b8a539052ea7a77fabd126aabaea
rainseemstopzgz <zgz2233@outlook.com>
2022-03-08 18:44:18 +0800

    第二次修改

--- backend/router.js
+++ backend/router.js
@@ -1,11 +1,8 @@
-/*
- * 该文件是运行在 Node.js 端的，获取数据的基本的思路就是后端代理，即提供接口路由供前端页面使用，然后在路由内部，我们接收到前端请求后，再发送 HTTP 请求到第三方服务接口，携带相应的请求参数，包括签名的参数字段等等。
- * 对于从第三方接口返回的数据，我们会做一层数据处理，最终提供给前端的数据前端可以直接使用，无需再处理。这样也比较符合真实企业项目的开发规范，即数据的处理放在后端做，前端只做数据渲染和交互。
- */
 const axios = require('axios')
+const { Base64 } = require('js-base64')
 const pinyin = require('pinyin')
-const Base64 = require('js-base64').Base64
-// 获取签名方法
+
+// const Base64 = require('js-base64').Base64
 const getSecuritySign = require('./sign')
 
 const ERR_OK = 0
@@ -38,18 +35,6 @@ function getUid() {
   return '' + Math.round(2147483647 * Math.random()) * t % 1e10
 }
 
-// 对 axios get 请求的封装
-// 修改请求的 headers 值，合并公共请求参数
-function get(url, params) {
-  return axios.get(url, {
-    headers: {
-      referer: 'https://y.qq.com/',
-      origin: 'https://y.qq.com/'
-    },
-    params: Object.assign({}, commonParams, params)
-  })
-}
-
 // 对 axios post 请求的封装
 // 修改请求的 headers 值
 function post(url, params) {
@@ -59,6 +44,16 @@ function post(url, params) {
       origin: 'https://y.qq.com/',
       'Content-Type': 'application/x-www-form-urlencoded'
     }
+  })
+}
+
+function get(url, params) {
+  return axios.get(url, {
+    headers: {
+      referer: 'https://y.qq.com/',
+      origin: 'https://y.qq.com/'
+    },
+    params: Object.assign({}, commonParams, params)
   })
 }
 
@@ -90,6 +85,7 @@ function handleSongList(list) {
 
   return songList
 }
+
 // 合并多个歌手的姓名
 function mergeSinger(singer) {
   const ret = []
@@ -102,10 +98,8 @@ function mergeSinger(singer) {
   return ret.join('/')
 }
 
-// 注册后端路由
 function registerRouter(app) {
   registerRecommend(app)
-
   registerSingerList(app)
   registerSingerDetail(app)
   registerSongsUrl(app)
@@ -114,15 +108,16 @@ function registerRouter(app) {
   registerTopList(app)
   registerTopDetail(app)
   registerHotKeys(app)
-
   registerSearch(app)
 }
+
 // 注册推荐列表接口路由
 function registerRecommend(app) {
   app.get('/api/getRecommend', (req, res) => {
-    // 第三方服务接口 url
+    // 第三方服务接口url
     const url = 'https://u.y.qq.com/cgi-bin/musics.fcg'
 
+    // 构造请求data参数
     // 构造请求 data 参数
     const data = JSON.stringify({
       comm: { ct: 24 },
@@ -134,12 +129,12 @@ function registerRecommend(app) {
       focus: { module: 'music.musicHall.MusicHallPlatform', method: 'GetFocus', param: {} }
     })
 
-    // 随机数值
+     // 随机数值
     const randomVal = getRandomVal('recom')
-    // 计算签名值
+     // 计算签名值
     const sign = getSecuritySign(data)
 
-    // 发送 get 请求
+    // 发送请求
     get(url, {
       sign,
       '-': randomVal,
@@ -155,20 +150,19 @@ function registerRecommend(app) {
           10014: 'https://y.qq.com/n/yqq/playlist/',
           10012: 'https://y.qq.com/n/yqq/mv/v/'
         }
-        // 最多获取 10 条数据
+        // 最多获取10调数据
         const len = Math.min(focusList.length, 10)
         for (let i = 0; i < len; i++) {
           const item = focusList[i]
           const sliderItem = {}
-          // 单个轮播图数据包括 id、pic、link 等字段
+          // 单个轮播图数据包括id、pic、link等字段
           sliderItem.id = item.id
           sliderItem.pic = item.cover
           if (jumpPrefixMap[item.jumptype]) {
-            sliderItem.link = jumpPrefixMap[item.jumptype] + (item.subid || item.id) + '.html'
+            sliderItem.link = jumpPrefixMap[item.jumptype] + (item.subid || item.id) + 'html'
           } else if (item.jumptype === 3001) {
             sliderItem.link = item.id
           }
-
           sliders.push(sliderItem)
         }
 
@@ -178,7 +172,7 @@ function registerRecommend(app) {
         for (let i = 0; i < albumList.length; i++) {
           const item = albumList[i]
           const albumItem = {}
-          // 推荐歌单数据包括 id、username、title、pic 等字段
+          // 处理推荐歌单数据包括id、username、title、pic等字段
           albumItem.id = item.content_id
           albumItem.username = item.username
           albumItem.title = item.title
@@ -187,23 +181,21 @@ function registerRecommend(app) {
           albums.push(albumItem)
         }
 
-        // 往前端发送一个标准格式的响应数据，包括成功错误码和数据
-        setTimeout(() => {
-          res.json({
-            code: ERR_OK,
-            result: {
-              sliders,
-              albums
-            }
-          })
-        }, 1000)
+        // 往前端发送一个标准格式的相应数据，包括错误码和成功数据
+        res.json({
+          code: ERR_OK,
+          result: {
+            sliders,
+            albums
+          }
+        })
       } else {
         res.json(data)
       }
     })
   })
 }
-// 注册歌手列表接口路由
+
 function registerSingerList(app) {
   app.get('/api/getSingerList', (req, res) => {
     const url = 'https://u.y.qq.com/cgi-bin/musics.fcg'
@@ -217,6 +209,7 @@ function registerSingerList(app) {
         param: { area: -100, sex: -100, genre: -100, index: -100, sin: 0, cur_page: 1 }
       }
     })
+
     const randomKey = getRandomVal('getUCGI')
     const sign = getSecuritySign(data)
 
@@ -235,11 +228,13 @@ function registerSingerList(app) {
             list: map(singerList.slice(0, 10))
           }
         }
+
         singerList.forEach((item) => {
           const p = pinyin(item.singer_name)
           if (!p || !p.length) {
             return
           }
+
           const key = p[0][0].slice(0, 1).toUpperCase()
           if (key) {
             if (!singerMap[key]) {
@@ -251,20 +246,23 @@ function registerSingerList(app) {
             singerMap[key].list.push(map([item])[0])
           }
         })
+
         const hot = []
         const letter = []
 
         for (const key in singerMap) {
           const item = singerMap[key]
-          if (item.title.match(/[a-zA-z]/)) {
+          if (item.title.match(/[a-zA-Z]/)) {
             letter.push(item)
           } else if (item.title === HOT_NAME) {
             hot.push(item)
           }
         }
+
         letter.sort((a, b) => {
           return a.title.charCodeAt(0) - b.title.charCodeAt(0)
         })
+
         res.json({
           code: ERR_OK,
           result: {
@@ -276,7 +274,8 @@ function registerSingerList(app) {
       }
     })
   })
-  // 做一层数据映射，构造单个 singer 数据结构
+
+  // 做一层数据映射，构成单个 singer 数据结构
   function map(singerList) {
     return singerList.map((item) => {
       return {
@@ -288,6 +287,7 @@ function registerSingerList(app) {
     })
   }
 }
+
 // 注册歌手详情接口路由
 function registerSingerDetail(app) {
   app.get('/api/getSingerDetail', (req, res) => {
@@ -329,14 +329,12 @@ function registerSingerDetail(app) {
   })
 }
 
-// 注册歌曲 url 获取接口路由
-// 因为歌曲的 url 每天都在变化，所以需要单独的接口根据歌曲的 mid 获取
 function registerSongsUrl(app) {
   app.get('/api/getSongsUrl', (req, res) => {
     const mid = req.query.mid
 
     let midGroup = []
-    // 第三方接口只支持最多处理 100 条数据，所以如果超过 100 条数据，我们要把数据按每组 100 条切割，发送多个请求
+
     if (mid.length > 100) {
       const groupLen = Math.ceil(mid.length / 100)
       for (let i = 0; i < groupLen; i++) {
@@ -346,10 +344,8 @@ function registerSongsUrl(app) {
       midGroup = [mid]
     }
 
-    // 以歌曲的 mid 为 key，存储歌曲 URL
     const urlMap = {}
 
-    // 处理返回的 mid
     function process(mid) {
       const data = {
         req_0: {
@@ -391,14 +387,11 @@ function registerSongsUrl(app) {
       })
     }
 
-    // 构造多个 Promise 请求
     const requests = midGroup.map((mid) => {
       return process(mid)
     })
 
-    // 并行发送多个请求
     return Promise.all(requests).then(() => {
-      // 所有请求响应完毕，urlMap 也就构造完毕了
       res.json({
         code: ERR_OK,
         result: {
@@ -436,6 +429,7 @@ function registerLyric(app) {
 }
 
 // 注册歌单专辑接口
+
 function registerAlbum(app) {
   app.get('/api/getAlbum', (req, res) => {
     const data = {
@@ -694,5 +688,4 @@ function registerSearch(app) {
     })
   })
 }
-
 module.exports = registerRouter
